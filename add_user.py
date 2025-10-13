9b42f21f9b7a633235f6832f4af7e698102da15e
njabulo moshifa <107797576+moshifa10@users.noreply.github.com>
2025-10-14 00:44:08 +0200

    added something great

--- add_user.py
+++ add_user.py
@@ -1,8 +1,16 @@
 import json
 from rules import rules
+import sys
+import time
+import os
+from plyer import notification
 # Here I will be adding users 
 # now I have to add numbers and email -> valid ones
 def add_user()-> dict:
+    os.system('cls' or 'clear')
+    print()
+    print("----------------------------   Creating account 🚀🚀  ----------------------------")
+    print()
     while True:
         try:
             surname = input("Your Surname?: ").title()
@@ -12,6 +20,10 @@ def add_user()-> dict:
                 print("Surname or Name, Cannot be empty or less than 3 words. Try again !!!!!")
                 print() 
                 continue
+            for i in range(10):
+                if str(i) in name or str(i) in surname:
+                    raise ValueError
+
             if len(str(id_num)) < 12 or len(str(id_num)) > 13:
                 print("ID cannot be less than 13, or greater than 13 Please try again")
                 print()
@@ -29,6 +41,9 @@ def add_user()-> dict:
 # Now my plan is to prompt for their pin and user name or passcode 
 
 def username_pin(data: dict) -> dict:
+    os.system('cls' or 'clear')
+    print()
+    print("----------------------------   Creating Pin 🚀🚀  ----------------------------")
     print()
     print("Lets create Pin for account to be secured")
     print("Remember your pin please!!!!!!!!!")
@@ -42,11 +57,11 @@ def username_pin(data: dict) -> dict:
     while True:
         print()
         try:
-            user_pin = input("Create Your pin: ")
-            if len(user_pin) < 4 or len(user_pin) > 4:
+            user_pin = int(input("Create Your pin: "))
+            if len(str(user_pin)) < 4 or len(str(user_pin)) > 4:
                 print("Your pin should be 4 digits ONLY!!!!!!!! ")
                 continue
-            if user_pin.count(user_pin[0]) > 2 or user_pin.count(user_pin[1]) > 2:
+            if str(user_pin).count(str(user_pin)[0]) > 2 or str(user_pin).count(str(user_pin)[1]) > 2:
                 print(f"You repeated a digit more than twice which is not allowed !!!!!")
                 continue
 
@@ -55,7 +70,22 @@ def username_pin(data: dict) -> dict:
             print("Your pin should be digits only pay attention please!!!!")
             continue
         else:
-            print("--------------------IMPORTANT--------------------------")
+            print("Loading", end="")
+            start_time = time.time()
+            while time.time() - start_time < 5:
+                print(".", end="")
+                sys.stdout.flush()
+                time.sleep(0.5)
+            print("\nCreated account successfully 🚀 🚀")
+            while True:
+               notification.notify(
+                   title = "ALERT!!!!",
+                   message = f"{data["name"]} Account created successfully.",
+                   timeout = 10
+               )
+               time.sleep(3) 
+               break
+            print("--------------------------   IMPORTANT   --------------------------")
             print("Remember username is your ID")
             data["pin"] = user_pin
             return data
--- main.py
+++ main.py
@@ -1,10 +1,31 @@
 from add_to_json import add_to_json
 from add_user import add_user , username_pin
+import os
+
 
 def main():
-    user_info = add_user()
-    user_pin = username_pin(user_info)
-    add_to_json(user_pin)
+    options = ["Create account", "Login", "Edit account", "Delete account"]
+    
+    print("-------------- Welcome to Majozi's Bank System --------------")
+    print()
+    print("We have options to choose from")
+    print()
+    while True:
+        try:
+            print("Just select any option from 1-4")
+            for count, option in enumerate(options, start=1):
+                print(f"{count}) {option}")
 
+            user = int(input("Your option: "))
+            if user == 1:
+                user_info = add_user()
+                user_pin = username_pin(user_info)
+                add_to_json(user_pin)
+                break
+
+        except ValueError:
+            print("Please provide a digit and 1-4")
+
+        
 if __name__ == "__main__":
     main()
